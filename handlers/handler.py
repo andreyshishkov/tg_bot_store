296862da610a3914a6ab471bd5564e2a33c9a635
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-11-10 22:54:31 +0300

    Add abstract class Handler in handler.py

---
+++ handlers/handler.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from markup.markup import Keyboards
+from data_base.dbalchemy import DBManager
+
+
+class Handler(ABC):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.keyboards = Keyboards()
+        self.BD = DBManager()
+
+    @abstractmethod
+    def handle(self):
+        pass
