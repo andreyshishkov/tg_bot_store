f554d2ea7d1246bfc653de543d1e018a138470e3
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-11-12 04:37:44 +0300

    Add start_menu func

--- markup/markup.py
+++ markup/markup.py
@@ -1,4 +1,4 @@
-from telebot.types import KeyboardButton
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 from settings import config
 from data_base.dbalchemy import DBManager
 
@@ -12,6 +12,25 @@ class Keyboards:
         self.markup = None
         self.DB = DBManager()
 
-    def set_btn(self, name, step=0, quantity=0):
+    @staticmethod
+    def set_btn(name, step=0, quantity=0):
         return KeyboardButton(config.KEYBOARD.get(name))
-    
+
+    def start_menu(self):
+        """
+        Создает разметку в основном меню
+        """
+        self.markup = ReplyKeyboardMarkup(True, True)
+        itm_btn_1 = self.set_btn('CHOOSE GOODS')
+        itm_btn_2 = self.set_btn('INFO')
+        itm_btn_3 = self.set_btn('SETTINGS')
+
+        self.markup.row(itm_btn_1)
+        self.markup.row(itm_btn_2,itm_btn_3)
+        return self.markup
+
+
+    def info_menu(self):
+        """
+        Создает разметку в меню info
+        """
