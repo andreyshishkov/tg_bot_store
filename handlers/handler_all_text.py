70a60fee008b0ccd466c8e4c959389533d5bf2d6
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-11-12 05:50:29 +0300

    Add HandleAllText

--- handlers/handler_all_text.py
+++ handlers/handler_all_text.py
@@ -1,4 +1,6 @@
 from handlers.handler import Handler
+from settings import config
+from settings.message import MESSAGES
 
 
 class HandlerAllText(Handler):
@@ -21,3 +23,24 @@ class HandlerAllText(Handler):
             parse_mode='HTML',
             reply_markup=self.keyboards.settings_menu()
         )
+
+    def pressed_btn_back(self, message):
+        self.bot.send_message(
+            message.chat.id,
+            'Вы вернулись назад',
+            reply_markup=self.keyboards.start_menu()
+        )
+
+    def handle(self):
+
+        @self.bot.message_handler()
+        def handle(message):
+
+            if message.text  == config.KEYBOARD.get('INFO'):
+                self.pressed_btn_info(message)
+
+            if message.text == config.KEYBOARD.get('SETTINGS'):
+                self.pressed_btn_settings(message)
+
+            if message.text == config.KEYBOARD.get('<<'):
+                self.pressed_btn_back(message)
