22d35eb1d427da0b4fb13e2548a34f79d78f9355
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-11-20 21:45:57 +0300

    Add orders_menu

--- markup/markup.py
+++ markup/markup.py
@@ -60,6 +60,25 @@ class Keyboards:
         self.markup.row(self.set_btn('<<'), self.set_btn('ORDER'))
         return self.markup
 
+    def orders_menu(self, step, quantity):
+        self.markup = ReplyKeyboardMarkup(True, True)
+        itm_btn_1 = self.set_btn('X', step, quantity)
+        itm_btn_2 = self.set_btn('DOWN', step, quantity)
+        itm_btn_3 = self.set_btn('AMOUNT PRODUCT', step, quantity)
+        itm_btn_4 = self.set_btn('UP', step, quantity)
+
+        itm_btn_5 = self.set_btn('BACK_STEP', step, quantity)
+        itm_btn_6 = self.set_btn('AMOUNT ORDERS', step, quantity)
+        itm_btn_7 = self.set_btn('NEXT_STEP', step, quantity)
+        itm_btn_8 = self.set_btn('APPLY', step, quantity)
+        itm_btn_9 = self.set_btn('<<', step, quantity)
+
+        self.markup.row(itm_btn_1, itm_btn_2, itm_btn_3, itm_btn_4)
+        self.markup.row(itm_btn_5, itm_btn_6, itm_btn_7)
+        self.markup.row(itm_btn_8, itm_btn_9)
+
+        return self.markup
+
     @staticmethod
     def set_inline_btn(name):
         return InlineKeyboardButton(
